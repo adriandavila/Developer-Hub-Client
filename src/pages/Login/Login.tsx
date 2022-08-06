54a326727d28246686b78bf8cd26a5b6421560d0
Adrian Davila <akdavila03@gmail.com>
2022-08-06 19:08:51 -0400

    Styling, assets, login page

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,21 @@
+import IconButton from "@mui/material/IconButton";
+import GoogleIcon from "@mui/icons-material/Google";
+
+import styles from "./login.module.scss";
+
+function Login() {
+  return (
+    <>
+      <div className={styles.background}>
+        <div className={styles.mainContainer}>
+          <h2 className={styles.welcomeText}>Welcome</h2>
+          <IconButton href="/" color="inherit">
+            <GoogleIcon />
+          </IconButton>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
