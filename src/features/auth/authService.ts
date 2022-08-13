dfe38d32791dcdcc7567cd33755fdeeda8835943
Adrian Davila <akdavila03@gmail.com>
2022-08-13 15:14:52 -0400

    Code readability change

--- src/features/auth/authService.ts
+++ src/features/auth/authService.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const API_URL = "/api/auth/";
+const API_URL = "/api/auth";
 
 // Login user
 const login = async (userData: any) => {
-  const response = await axios.post(`${API_URL}login`, userData);
+  const response = await axios.post(`${API_URL}/login`, userData);
 
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(userData));
+    console.log("Setting user to", userData);
   }
 
   return response.data;
