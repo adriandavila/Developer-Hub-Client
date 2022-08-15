54dc8e2c3faf6d4e3069d3efaa3604f6551d2920
Adrian Davila <akdavila03@gmail.com>
2022-08-15 00:42:36 -0400

    Profile information styling

--- src/components/Misc/ProfileInformation.tsx
+++ src/components/Misc/ProfileInformation.tsx
@@ -8,18 +8,32 @@ import UserAvatar from "./UserAvatar";
 function ProfileInformation() {
   const { user } = useSelector((state: any) => state.auth);
   return (
-    <Box sx={{ display: "flex", alignItems: "center" }}>
+    <Box
+      sx={{ display: "flex", alignItems: "center", textOverflow: "ellipsis" }}
+    >
       <Box sx={{ mr: 1 }}>
         <UserAvatar />
       </Box>
       <div>
         <Typography
-          sx={{ fontFamily: "'DynaPuff', cursive", fontSize: "18px" }}
+          sx={{
+            fontFamily: "'Poppins', sans-serif",
+            fontSize: "17px",
+            fontWeight: 500,
+            lineHeight: 1.3,
+          }}
+          component="div"
         >
           {user.name}
         </Typography>
         <Typography
-          sx={{ fontFamily: "'DynaPuff', cursive", fontSize: "13px" }}
+          sx={{
+            fontFamily: "'Poppins', sans-serif",
+            fontSize: "12px",
+            lineHeight: 1.3,
+            fontWeight: 300,
+          }}
+          component="div"
         >
           {user.email}
         </Typography>
