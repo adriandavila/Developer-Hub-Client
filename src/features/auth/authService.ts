f5d4eca0cc7982c5f10165e303ef727b5d169b0e
Adrian Davila <akdavila03@gmail.com>
2022-08-15 00:06:14 -0400

    Fixed more authentication woes

--- src/features/auth/authService.ts
+++ src/features/auth/authService.ts
@@ -7,7 +7,7 @@ const login = async (userData: any) => {
   const response = await axios.post(`${API_URL}/login`, userData);
 
   if (response.data) {
-    localStorage.setItem("user", JSON.stringify(userData));
+    localStorage.setItem("user", JSON.stringify(response.data));
   }
 
   return response.data;
