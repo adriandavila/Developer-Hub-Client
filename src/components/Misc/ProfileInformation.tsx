af5caf8ee680e13edbee92fb23cf8dcc54fcf776
Adrian Davila <akdavila03@gmail.com>
2022-08-15 00:18:31 -0400

    Profile information component now retrieves user data from state

--- src/components/Misc/ProfileInformation.tsx
+++ src/components/Misc/ProfileInformation.tsx
@@ -1,18 +1,28 @@
 // MUI Components
 import { Box, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 
 // Custom components
 import UserAvatar from "./UserAvatar";
 
 function ProfileInformation() {
+  const { user } = useSelector((state: any) => state.auth);
   return (
     <Box sx={{ display: "flex", alignItems: "center" }}>
       <Box sx={{ mr: 1 }}>
         <UserAvatar />
       </Box>
       <div>
-        <Typography>Adrian Davila</Typography>
-        <Typography>adrian@h2analytics.ca</Typography>
+        <Typography
+          sx={{ fontFamily: "'DynaPuff', cursive", fontSize: "18px" }}
+        >
+          {user.name}
+        </Typography>
+        <Typography
+          sx={{ fontFamily: "'DynaPuff', cursive", fontSize: "13px" }}
+        >
+          {user.email}
+        </Typography>
       </div>
     </Box>
   );
