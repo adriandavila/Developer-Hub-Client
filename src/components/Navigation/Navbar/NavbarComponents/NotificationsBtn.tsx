78c7924b941c42c65169d30b84b2c95b993b5472
Adrian Davila <akdavila03@gmail.com>
2022-08-21 21:32:02 -0400

    Make notifications btn in navbar larger

--- src/components/Navigation/Navbar/NavbarComponents/NotificationsBtn.tsx
+++ src/components/Navigation/Navbar/NavbarComponents/NotificationsBtn.tsx
@@ -3,7 +3,11 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 
 function NotificationsBtn() {
   return (
-    <IconButton aria-label="show 17 new notifications" color="inherit">
+    <IconButton
+      size="large"
+      aria-label="show 17 new notifications"
+      color="inherit"
+    >
       <Badge badgeContent={17} color="error">
         <NotificationsOutlinedIcon />
       </Badge>
