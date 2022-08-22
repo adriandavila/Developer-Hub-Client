08b7cf9176d715d6a03688f003b6954259ce47e4
Adrian Davila <akdavila03@gmail.com>
2022-08-21 23:21:03 -0400

    Start notifications menu

--- src/components/Navigation/Navbar/NavbarComponents/NotificationsBtn.tsx
+++ src/components/Navigation/Navbar/NavbarComponents/NotificationsBtn.tsx
@@ -1,18 +1,94 @@
-import { IconButton, Badge } from "@mui/material";
+// React
+import * as React from "react";
+import { useState } from "react";
+
+// MUI Components
+import { IconButton, Badge, Menu, Typography } from "@mui/material";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 
-function NotificationsBtn() {
+import Box from "@mui/material/Box";
+import { useTheme } from "src/hooks/useTheme";
+
+export default function NotificationsMenu() {
+  const [anchorElNotifications, setAnchorElNotifications] =
+    useState<null | HTMLElement>(null);
+
+  const handleOpenNotificationsMenu = (
+    event: React.MouseEvent<HTMLElement>
+  ) => {
+    setAnchorElNotifications(event.currentTarget);
+  };
+
+  const handleCloseNotificationsMenu = () => {
+    setAnchorElNotifications(null);
+  };
+
+  const theme = useTheme();
+
+  const menuDropShadow = {
+    light: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+    dark: "drop-shadow(0px 2px 8px rgba(255,255,255,0.1))",
+  };
+
   return (
-    <IconButton
-      size="large"
-      aria-label="show 17 new notifications"
-      color="inherit"
-    >
-      <Badge badgeContent={17} color="error">
-        <NotificationsOutlinedIcon />
-      </Badge>
-    </IconButton>
+    <Box sx={{ flexGrow: 0 }}>
+      <IconButton
+        size="large"
+        aria-label="show 17 new notifications"
+        color="inherit"
+        onClick={handleOpenNotificationsMenu}
+      >
+        <Badge badgeContent={17} color="error">
+          <NotificationsOutlinedIcon />
+        </Badge>
+      </IconButton>
+
+      <Menu
+        sx={{ mt: "45px" }}
+        id="menu-appbar"
+        anchorEl={anchorElNotifications}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorElNotifications)}
+        onClose={handleCloseNotificationsMenu}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: menuDropShadow[theme.theme as "light" | "dark"],
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+      >
+        <Box sx={{ width: 350, px: 2 }}>
+          <Typography>Notifications</Typography>
+        </Box>
+      </Menu>
+    </Box>
   );
 }
-
-export default NotificationsBtn;
