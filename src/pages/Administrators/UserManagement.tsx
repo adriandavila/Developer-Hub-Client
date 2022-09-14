888d8e5d6c6013d06cbca6cf48e495f7ea7f0696
Adrian Davila <akdavila03@gmail.com>
2022-09-14 10:46:00 -0400

    Add UserManagementTable to UserManagement page

--- src/pages/Administrators/UserManagement.tsx
+++ src/pages/Administrators/UserManagement.tsx
@@ -1,10 +1,12 @@
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import PageTitle from "src/components/Misc/PageTitle";
+import UserManagementTable from "src/components/User/UserManagementTable";
 
 function UserManagement() {
   return (
     <Container maxWidth={false} sx={{ p: 2, m: 0 }}>
       <PageTitle title="User Management" />
+      <UserManagementTable />
     </Container>
   );
 }
