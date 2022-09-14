a9581eec7fd1144bafeb2c7f595338fa9fe580b8
Adrian Davila <akdavila03@gmail.com>
2022-09-14 13:36:29 -0400

    Prepare activity table

--- src/pages/Developers/ApiMonitor.tsx
+++ src/pages/Developers/ApiMonitor.tsx
@@ -1,10 +1,38 @@
-import { Container } from "@mui/material";
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import PageTitle from "src/components/Misc/PageTitle";
 
 function ApiMonitor() {
   return (
     <Container maxWidth={false} sx={{ p: 2, m: 0 }}>
       <PageTitle title="API Monitor" />
+      <h1>Chart</h1>
+      <PageTitle title="Activity" />
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell />
+              <TableCell>Date / Time</TableCell>
+              <TableCell>Route</TableCell>
+              <TableCell align="right">Method</TableCell>
+              <TableCell align="right">HTTP</TableCell>
+              <TableCell align="right">Error</TableCell>
+              <TableCell align="right">Status</TableCell>
+              <TableCell align="right">Ticket</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody></TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 }
