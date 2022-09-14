a6a6ea8f638d04f21c6c5561634627cc4739b301
Adrian Davila <akdavila03@gmail.com>
2022-09-14 14:58:28 -0400

    Fix activity table styling

--- src/components/ApiMonitor/ActivityTable.tsx
+++ src/components/ApiMonitor/ActivityTable.tsx
@@ -62,7 +62,7 @@ function Row(props: { row: any }) {
         </TableCell>
       </TableRow>
       <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={8}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
               <Typography variant="h6" gutterBottom component="div">
